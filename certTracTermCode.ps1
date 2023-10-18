2ac9991da896a84fbb3867ffbc62f29121a2ddcc
TexasHolden <70984537+TexasHolden@users.noreply.github.com>
2023-10-18 16:54:14 -0400

    more setup

--- certTracTermCode.ps1
+++ certTracTermCode.ps1
@@ -1,3 +1,6 @@
 Write-Host "Trying to invoke activate"
-$activate = 'C:\Users\holde\CertTrac_env\Scripts\activate.ps1'
+$activate = 'C:\Users\holde\venv\Scripts\activate.ps1'
 . $activate
+#to do the thing, run this and then run
+#  python manage.py runserver
+#ctrl c to stop it  
