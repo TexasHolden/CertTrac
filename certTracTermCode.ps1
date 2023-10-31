54691e2613f98785af2730aa0795f935aad4dab2
J03P <32021675+J03P@users.noreply.github.com>
2023-10-31 00:10:18 +0000

    Delete certTracTermCode.ps1

--- certTracTermCode.ps1
+++
@@ -1,6 +0,0 @@
-Write-Host "Trying to invoke activate"
-$activate = 'C:\Users\holde\venv\Scripts\activate.ps1'
-. $activate
-#to do the thing, run this and then run
-#  python manage.py runserver
-#ctrl c to stop it  
