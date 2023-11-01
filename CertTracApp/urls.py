2d7821d0ee37bb69f684efefe3bfc4547c024fdf
Aidanman2 <aidankahns@gmail.com>
2023-11-01 17:53:17 -0400

    added 25

--- CertTracApp/urls.py
+++ CertTracApp/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('query_results/', views.query_results, name='query_results'),
+<<<<<<< Updated upstream
     path('session', views.session, name='session')
+=======
+    path('25', views.page25, name='25')
+>>>>>>> Stashed changes
 ]
