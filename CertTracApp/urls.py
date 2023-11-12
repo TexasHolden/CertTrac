aa17078046ac99ad59bf1b6fc2413f3fb44b0338
kwireman <89817020+kwireman@users.noreply.github.com>
2023-11-12 16:51:04 -0500

    Revert "Revert "Merge branch 'test' of https://github.com/TexasHolden/CertTrac into test""
    This reverts commit 948c98128d102dece68b0077051689daa891fb09.

--- CertTracApp/urls.py
+++ CertTracApp/urls.py
@@ -9,8 +9,6 @@ urlpatterns = [
     path('help', views.help, name='help'),
         # New URL patterns for the navigation bar
     path('view_edit_tutor_hours/', views.view_edit_tutor_hours, name='view_edit_tutor_hours'),
-    path('add_tutor_hours/', views.add_tutor_hours, name='add_tutor_hours'),
-    path('edit_tutor_hours/<int:tutor_hours_id>/', views.edit_tutor_hours, name='edit_tutor_hours'),
     path('input_hours/', views.input_hours, name='input_hours'),
     path('input_completed_courses/', views.input_completed_courses, name='input_completed_courses'),
     path('add_remove_tutors/', views.add_remove_tutors, name='add_remove_tutors'),
